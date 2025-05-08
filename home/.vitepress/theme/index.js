8b6e9aacc9754fdb9d348a07b81b078cea6d990b
Cam <5cam@proton.me>
2025-05-08 09:59:28 +0000

    :nut_and_bolt: dir names & scripts

--- home/.vitepress/theme/index.js
+++ home/.vitepress/theme/index.js
@@ -1,14 +1,14 @@
 import 'tailwindcss/dist/tailwind.css'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
-// import Breezy from '/@breezy/index.js'
+import Lib from '../../lib/index.js'
 import Browser from '../components/Browser.vue'
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app, router, siteData }) {
-    // app.use(Breezy)
+    app.use(Lib)
     app.component('browser', Browser)
   }
 }
